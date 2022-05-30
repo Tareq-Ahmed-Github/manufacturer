918f7f9b06855f8497f00775c794a1b79a570029
unknown <tareqrobin57@gmail.com>
2022-05-30 23:52:34 +0600

    private route

--- src/Components/Routes/PrivateRoutes.js
+++ src/Components/Routes/PrivateRoutes.js
@@ -1,3 +1,4 @@
+import DashboardSidebar from '../Private/Dashboard/DashboardSidebar';
 import MyOrder from '../Private/Dashboard/MyOrder';
 import Users from '../Private/Dashboard/Users';
 import Purchase from '../Private/Purchase';
@@ -6,6 +7,7 @@ import ToolsAll from './../Private/ToolsAll';
 export const privateRoutes = [
 
   { path: "/toolsall", name: "Tools", Component: ToolsAll },
+  { path: "/dashboard", name: "Dashboard", Component: DashboardSidebar },
   { path: "/purchase", name: "Purchase", Component: Purchase },
   { path: "/dashboard/myorder", name: "MyOrder", Component: MyOrder },
   { path: "/dashboard/users", name: "users", Component: Users }
